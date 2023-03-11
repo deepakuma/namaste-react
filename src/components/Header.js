65649933857911690f1b716f1a03a23fd2109a53
deepakuma <deepakumari22.3.1999@gmail.com>
2023-03-11 09:09:09 +0530

    Chapter 06

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,10 @@
+import { useState } from "react";
+
+const loggedInUser = () =>{
+    // API call to check authentication
+    return true;
+}
+
 const Title = () => (
     <a href="/">
     <img className="logo" alt="logo" src="https://yt3.googleusercontent.com/ytc/AL5GRJXudT76175T4x4n7eslWM1YkgNLHDSSqfXGoadl=s900-c-k-c0x00ffffff-no-rj"/>
@@ -5,6 +12,9 @@ const Title = () => (
 ) ;
 
 const Header = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
     <div className="header">
         <Title />
@@ -16,6 +26,7 @@ const Header = () => {
                 <li>Cart</li>
             </ul>
         </div>
+        {isLoggedIn? <button onClick={()=> setIsLoggedIn(false)}>Logout</button> : <button onClick={()=> setIsLoggedIn(true)}>Login</button>}
 
     </div>
     );
